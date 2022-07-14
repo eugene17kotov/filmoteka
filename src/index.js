7dd925758eeba9f6da7a0e856d423c8b56f40e67
Yevhenii Kobets <eu.kobets@gmail.com>
2022-07-14 15:56:40 +0300

    update base structure

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+// Styles
 import './sass/main.scss';
+
+// Scripts
+import './js/search';
+import './js/movie-modal';
+import './js/pagination';
+import './js/team-modal';
+import './js/watched';
+import './js/queue';
+import './js/scroll-up';
---
+++ src/js/api/fetch-movie.js
@@ -0,0 +1,2 @@
+import axios from 'axios';
+import { API_KEY, TREND_URL, SEARCH_URL, ID_URL } from './api-vars';
