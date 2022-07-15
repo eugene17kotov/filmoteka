79518394cf99558180a6eda54a50060586022ee4
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-07-15 22:19:17 +0300

    add trend movies render at homepage

---
+++ src/js/render-movie-cards.js
@@ -0,0 +1,19 @@
+import { getMovies } from './api/fetch-movie';
+import { createMovieMarkup } from './create-movie-markup';
+import { TREND_URL } from './api/api-vars';
+
+(async () => {
+  const trendMoviesList = (await getMovies(TREND_URL)).results;
+
+  renderMovieCards(trendMoviesList);
+})();
+
+function renderMovieCards(movies) {
+  const movieGalleryMarkup = movies
+    .map(movie => createMovieMarkup(movie))
+    .join('');
+
+  document
+    .querySelector('.gallery')
+    .insertAdjacentHTML('beforeend', movieGalleryMarkup);
+}
