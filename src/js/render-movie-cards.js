6317e3c6a4c683c0b305008916c836f40cb9e560
dkrasovskyi <dkrasovskyi@sardinasystems.com>
2022-07-16 19:20:19 +0300

    70%

--- src/js/render-movie-cards.js
+++ src/js/render-movie-cards.js
@@ -1,19 +1,25 @@
 import { getMovies } from './api/fetch-movie';
 import { createMovieMarkup } from './create-movie-markup';
-import { TREND_URL } from './api/api-vars';
+import { LAST_REQUESTED_URL, TREND_URL } from './api/api-vars';
+import { renderPagination } from './pagination.js';
 
 (async () => {
-  const trendMoviesList = (await getMovies(TREND_URL)).results;
+  const trendMoviesList = (await getMovies(TREND_URL));
 
-  renderMovieCards(trendMoviesList);
+  renderMovieCards(trendMoviesList.results);
+
+  // need to save last url to localstorage 
+  localStorage.setItem('LAST_REQUESTED_URL', TREND_URL)
+  // render pagination buttons
+  renderPagination(trendMoviesList.page, trendMoviesList.total_pages);
 })();
 
-function renderMovieCards(movies) {
+export function renderMovieCards(movies) {
   const movieGalleryMarkup = movies
     .map(movie => createMovieMarkup(movie))
     .join('');
-
   document
     .querySelector('.gallery')
-    .insertAdjacentHTML('beforeend', movieGalleryMarkup);
+    .innerHTML=movieGalleryMarkup;
+    // .insertAdjacentHTML('beforeend', movieGalleryMarkup);   // we need replace card, not append!
 }
