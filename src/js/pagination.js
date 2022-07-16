fe1246c89bfa137ce5f938d6e69be4cf6841702c
Victor8825 <victorrv2588@gmail.com>
2022-07-16 13:48:37 +0300

    in process

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,5 +1,7 @@
 import {getMovies} from "./api/fetch-movie";
-import { API_KEY, BASE_URL, TREND_URL, SEARCH_URL, ID_URL } from "./api/api-vars";
+// import {createMovieMarkup} from "./create-movie-markup";
+import {renderMovieCards} from "./render-movie-cards";
+import { API_KEY, BASE_URL, TREND_URL, SEARCH_URL, ID_URL,  BASE_IMG_URL } from "./api/api-vars";
 
 const paginationWrapRef = document.querySelector(".pagination-wrap");
 const arrowLeftBtnRef = document.querySelector(".arrow-left");
@@ -14,24 +16,15 @@ const btnThirdRef = document.querySelector("[data-index='3']");
 const btnFourthRef = document.querySelector("[data-index='4']");
 const btnFifthRef = document.querySelector("[data-index='5']");
 
-const galleryRef = document.querySelector(".gallery");
-const IMAGE_URL = "https://image.tmdb.org/t/p/w200";
+    function readyMarkup (url) {
+        getMovies(url)
+        .then(response => {
+            renderMovieCards(response.results)
+        })
+    }
 
-getMovies(TREND_URL)
-.then(response => 
-galleryRef.insertAdjacentHTML("afterbegin", buildMarkup(response.results)));
 
-function buildMarkup (responseFromServer) {
-  return responseFromServer.map( ({poster_path}) =>
-  `<a class="gallery__link" href="">
-        <img class="gallery__image" src="${IMAGE_URL}${poster_path}" alt="" loading="lazy" />
-    </a>
-    <div class="info">
-        <p class="info__item">Name</p>
-        <div class="info-detail">
-            <p class="info-detail__item">Genres</p>
-            <p class="info-detail__item">Year</p>
-        </div>
-    </div>`).join("");
-}
 
+
+    
+    
