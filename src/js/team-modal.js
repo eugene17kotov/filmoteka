07528b0daed5efec0024dd2ef78dcb9ef9d7545a
Yevhenii Kobets <eu.kobets@gmail.com>
2022-07-16 18:53:41 +0300

    intermidiate var

---
+++ src/js/team-modal.js
@@ -0,0 +1,28 @@
+import '../sass/main.scss';
+
+const teamRef = document.querySelector('.footer__link');
+const backdropTeamRef = document.querySelector('.backdrop-team');
+
+teamRef.addEventListener('click', onGoitteamClick);
+
+function onGoitteamClick() {
+  backdropTeamRef.classList.remove('backdrop-team--is-hidden');
+  document.addEventListener('keydown', onEscClick);
+  backdropTeamRef.addEventListener('click', onBackdropClick);
+}
+
+function onEscClick(event) {
+  event.preventDefault;
+
+  if (event.code === 'Escape') {
+    backdropTeamRef.classList.add('backdrop-team--is-hidden');
+    document.removeEventListener('keydown', onEscClick);
+  }
+}
+
+function onBackdropClick(event) {
+  if (!event.target.closest('.modal-team')) {
+    backdropTeamRef.classList.add('backdrop-team--is-hidden');
+    backdropTeamRef.removeEventListener('click', onBackdropClick);
+  }
+}
