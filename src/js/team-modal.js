b71ad4e32675727966ec6f610adb3473b1d59ee4
Yevhenii Kobets <eu.kobets@gmail.com>
2022-07-16 20:35:27 +0300

    some

--- src/js/team-modal.js
+++ src/js/team-modal.js
@@ -1,28 +1,30 @@
-import '../sass/main.scss';
-
 const teamRef = document.querySelector('.footer__link');
 const backdropTeamRef = document.querySelector('.backdrop-team');
 
-teamRef.addEventListener('click', onGoitteamClick);
+console.log(teamRef);
 
-function onGoitteamClick() {
-  backdropTeamRef.classList.remove('backdrop-team--is-hidden');
-  document.addEventListener('keydown', onEscClick);
-  backdropTeamRef.addEventListener('click', onBackdropClick);
-}
+// teamRef.addEventListener('click', onGoitteamClick);
 
-function onEscClick(event) {
-  event.preventDefault;
+// function onGoitteamClick(e) {
+//   e.preventDefault();
 
-  if (event.code === 'Escape') {
-    backdropTeamRef.classList.add('backdrop-team--is-hidden');
-    document.removeEventListener('keydown', onEscClick);
-  }
-}
+//   backdropTeamRef.classList.remove('backdrop-team--is-hidden');
+//   document.addEventListener('keydown', onEscClick);
+//   backdropTeamRef.addEventListener('click', onBackdropClick);
+// }
 
-function onBackdropClick(event) {
-  if (!event.target.closest('.modal-team')) {
-    backdropTeamRef.classList.add('backdrop-team--is-hidden');
-    backdropTeamRef.removeEventListener('click', onBackdropClick);
-  }
-}
+// function onEscClick(event) {
+//   event.preventDefault();
+
+//   if (event.code === 'Escape') {
+//     backdropTeamRef.classList.add('backdrop-team--is-hidden');
+//     document.removeEventListener('keydown', onEscClick);
+//   }
+// }
+
+// function onBackdropClick(event) {
+//   if (!event.target.closest('.modal-team')) {
+//     backdropTeamRef.classList.add('backdrop-team--is-hidden');
+//     backdropTeamRef.removeEventListener('click', onBackdropClick);
+//   }
+// }
