94c9e2fb933568373063ddfd92c4c04990bb39a2
denkach <denkach2211@gmail.com>
2022-07-16 16:24:39 +0200

    close modal

--- src/js/movie-modal.js
+++ src/js/movie-modal.js
@@ -1,1 +1,25 @@
+const refs = {
+  backdrop: document.querySelector('.backdrop'),
+  closeBtn: document.querySelector('button[data-dismiss="modal"]'),
+};
 
+const { backdrop, closeBtn } = refs;
+
+closeBtn.addEventListener('click', e => {
+  e.preventDefault();
+  backdrop.classList.add('visually-hidden');
+});
+
+window.addEventListener('keydown', e => {
+  e.preventDefault();
+  if (e.key === 'Escape') {
+    backdrop.classList.add('visually-hidden');
+  }
+});
+
+backdrop.addEventListener('click', e => {
+  if (!e.target.classList.contains('backdrop')) {
+    return;
+  }
+  backdrop.classList.add('visually-hidden');
+});
