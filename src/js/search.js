21584d13a7239a69648bdd5318cd4b998ede4323
Artem Makhov <makhovartem23@gmail.com>
2022-07-16 21:27:08 +0300

    add search movies

--- src/js/search.js
+++ src/js/search.js
@@ -1,10 +1,49 @@
-import { API_KEY, BASE_URL, TREND_URL, SEARCH_URL, ID_URL } from './api/api-vars';
-import {getMovies} from './api/fetch-movie.js';
+import { API_KEY, SEARCH_URL } from './api/api-vars';
+import { getMovies } from './api/fetch-movie.js';
+import { renderMovieCards } from './render-movie-cards';
+
+const refs = {
+    form: document.querySelector('.header__form'),
+    gallery: document.querySelector('.gallery'),
+}
+if (refs.form) {
+    refs.form.addEventListener('submit', onFormSubmit);
+}
 
 
-export function searchMovies(movie) {
+
+export function searchMovies(movie, pageNumber) {
     return getMovies(
-        `${SEARCH_URL}?api_key=${API_KEY}&query=${movie}`
+        `${SEARCH_URL}?api_key=${API_KEY}&query=${movie}&page=${pageNumber}`
     ); 
 }
+
+let searchText = '';
+
+export async function onFormSubmit(e) {
+    e.preventDefault();
+    
+    clearGallery();
+    
+    searchText = e.currentTarget.query.value.trim();
+    
+    const muvie = await searchMovies(searchText);
+
+    e.target.reset();
+    
+    renderMovieCards(muvie.results);
+    
+}
+
+function clearGallery() {
+    refs.gallery.innerHTML = '';
+}
+
+
+
+
+
+
+
+
 
