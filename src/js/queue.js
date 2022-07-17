ef99fe2ad349818bd7a07499169a79c461b3f0bd
denkach <denkach2211@gmail.com>
2022-07-17 23:34:48 +0200

    Update queue.js

--- src/js/queue.js
+++ src/js/queue.js
@@ -1,26 +1,57 @@
-const refs = {
-  queueBtn: document.querySelector('button.to-queue'),
-  gallery: document.querySelector('.library-gallery'),
-};
+// import fetchMovie from './api/fetch-movie';
+// import renderMovieCard from './render-movie-cards';
 
-const { queueBtn, gallery } = refs;
+// const refs = {
+//   queueBtn: document.querySelector('.to-queue'),
+//   gallery: document.querySelector('.library-gallery'),
+//   queueBtnLibrary: document.querySelector('button[data-action="queue"]'),
+// };
 
-queueBtn.addEventListener('click', onBtnQueueClick);
+// const { queueBtn, gallery, queueBtnLibrary } = refs;
 
-function inLocalStorage(id) {
-  if (!localStorage.getItem('queue').contains(id)) {
-    return false;
-  }
-  return true;
-}
+// console.log(document);
 
-function onBtnQueueClick() {
-  const id = document.querySelector('backdrop').id;
-  if (!inLocalStorage(id)) {
-    queueBtn.textContent = 'Remove to queue';
-    localStorage.setItem('queue', id);
-  } else {
-    queueBtn.textContent = 'Add to queue';
-    localStorage.removeItem('queue', id);
-  }
-}
+// // queueBtn.addEventListener('click', onBtnQueueClick);
+// // queueBtnLibrary.addEventListener('click', onBtnQueueLibraryClick);
+
+// const id = document.querySelector('backdrop').id;
+// console.log(id);
+
+// function inLocalStorage(id) {
+//   if (!localStorage.getItem('queue').contains(id)) {
+//     return false;
+//   }
+//   return true;
+// }
+
+// function onBtnQueueClick() {
+//   if (!inLocalStorage(id)) {
+//     queueBtn.textContent = 'Remove to queue';
+//     localStorage.setItem('queue', id);
+//   } else {
+//     queueBtn.textContent = 'Add to queue';
+//     localStorage.removeItem('queue', id);
+//   }
+// }
+
+// function onBtnQueueLibraryClick() {}
+
+// function createMovieMarkup(movie) {
+//   const { title, genre_ids, release_date, poster_path, vote_average } = movie;
+//   const year = release_date.slice(0, 4);
+//   const movieGenresList = getMovieGenresList(genre_ids).join(', ');
+
+//   return `<li>
+//             <a class="gallery__link" href="#">
+//               <img class="gallery__image" src="${BASE_IMG_URL}${poster_path}" alt="${title} movie poster" loading="lazy">
+
+//             <div class="info">
+//               <p class="info__item">${title}</p>
+//               <div class="info-detail">
+//                 <p class="info-detail__item">${movieGenresList}</p>
+//                 <p class="info-detail__item">${year} <span class="points">${vote_average}</span></p>
+//               </div>
+//             </div>
+//             </a>
+//           </li>`;
+// }
