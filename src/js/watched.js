22213ad3392e234d1ed6766cc2a7339c72f66c60
MariaKhmel <mariakhmel8@gmail.com>
2022-07-17 23:35:05 +0300

    add to watched (by ID)

--- src/js/watched.js
+++ src/js/watched.js
@@ -1,15 +1,8 @@
-import { API_KEY, SEARCH_URL } from './api/api-vars';
-import { getMovies } from './api/fetch-movie.js';
-import { createMovieMarkup } from './create-movie-markup';
-
 
 const refs = {
     addToWatchedButton: document.querySelector('.to-watched'),
-    watchedButton: document.querySelector('button[data-action="watched"]'),
-    film: document.querySelector('.modal h2'),
+    gallery: document.querySelector('.library-gallery'),
 }
-
-
 
 
 refs.addToWatchedButton.addEventListener('click', onAddToWatchedBtnClick);
@@ -17,64 +10,46 @@ refs.addToWatchedButton.addEventListener('click', onAddToWatchedBtnClick);
 
 
 function onAddToWatchedBtnClick() {
-   localStorage.setItem('watched', refs.film.textContent);
+  const id = document.querySelector('backdrop').id;
+  if (!isInLocalStorage(id)) {
     refs.addToWatchedButton.textContent = 'Remove From Watched';
-    refs.addToWatchedButton.classList.add('is-addedToWatchedFilms');
-   const watchedFilm = document.querySelector('.is-addedToWatchedFilms');
-    watchedFilm.addEventListener('click', onRemoveFromWatchedBtnClick);
-
+    localStorage.setItem('watched', id);
+  } else {
+    refs.addToWatchedButton.textContent = 'Add to watched';
+    localStorage.removeItem('watched', id);
+  }
+}
+function isInLocalStorage(id) {
+  if (!localStorage.getItem('watched').contains(id)) {
+    return false;
+  }
+  return true;
 }
 
+// function onAddToWatchedBtnClick() {
+//    localStorage.setItem('watched', refs.film.textContent);
+//     refs.addToWatchedButton.textContent = 'Remove From Watched';
+//     refs.addToWatchedButton.classList.add('is-addedToWatchedFilms');
+//    const watchedFilm = document.querySelector('.is-addedToWatchedFilms');
+//     watchedFilm.addEventListener('click', onRemoveFromWatchedBtnClick);
 
-function onRemoveFromWatchedBtnClick() {
+// }
+
+
+// function onRemoveFromWatchedBtnClick() {
   
-    refs.addToWatchedButton.classList.remove('is-addedToWatchedFilms');
-    refs.addToWatchedButton.textContent = 'Add to Watched';
-    localStorage.removeItem('watched');
-}
-
-
-function getStorage() {
-    let watched = localStorage.getItem('watched');
-    if (watched === null) {
-      return  watched = [];
-    } else {
-       return watched = JSON.parse(watched);
-    }
-}
-
-/////LIBRARY - WATCHED
-
-// refs.watchedButton.addEventListener('click', onWatchedBtnClick);
-
-// async function onWatchedBtnClick (){
-//     if (localStorage.getItem('key') !== null) {
-//    const  searchText = watchedMovies;
-//         const movie = await searchMovies(searchText);  
-//    console.log(movie)     
-//    renderMovieCards(movie.results);     
+//     refs.addToWatchedButton.classList.remove('is-addedToWatchedFilms');
+//     refs.addToWatchedButton.textContent = 'Add to Watched';
+//     localStorage.removeItem('watched');
 // }
 
 
-
-
+// function getStorage() {
+//     let watched = localStorage.getItem('watched');
+//     if (watched === null) {
+//       return  watched = [];
+//     } else {
+//        return watched = JSON.parse(watched);
+//     }
 // }
 
-
-
-
-// function searchMovies(movie) {
-//     return getMovies(
-//         `${SEARCH_URL}?api_key=${API_KEY}&query=${movie}`
-//     );
-// }
-
-// function renderMovieCards(movies) {
-//   const movieGalleryMarkup = movies
-//     .map(movie => createMovieMarkup(movie))
-//     .join('');
-
-//   document
-//     .querySelector('.library-gallery')
-//     .insertAdjacentHTML('beforeend', movieGalleryMarkup);
-// }
