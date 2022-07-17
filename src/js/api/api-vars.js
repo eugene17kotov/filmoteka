fcc92d0f133077c4fda1a3c92aa5c252daa1e099
veta41 <74262484+veta41@users.noreply.github.com>
2022-07-17 18:00:04 +0300

    add

--- src/js/api/api-vars.js
+++ src/js/api/api-vars.js
@@ -2,7 +2,9 @@ const API_KEY = '6f3a72a470b06bab99f8d69f54b4e2d3';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const TREND_URL = `${BASE_URL}/trending/movie/week?api_key=${API_KEY}`;
 const SEARCH_URL = `${BASE_URL}/search/movie`;
-const ID_URL = `${BASE_URL}/movie/`;
+// const ID_URL = `${BASE_URL}/movie/`;
+let id = 33333
+const ID_URL = `${BASE_URL}/movie/${id}?api_key=${API_KEY}&language=en-US`
 const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
 export { API_KEY, BASE_URL, TREND_URL, SEARCH_URL, ID_URL, BASE_IMG_URL };
--- src/js/movie-modal.js
+++ src/js/movie-modal.js
@@ -1,9 +1,16 @@
+import { getMovies } from './api/fetch-movie';
+import { ID_URL, BASE_IMG_URL } from './api/api-vars';
+
+
+
 const refs = {
   backdrop: document.querySelector('.backdrop'),
   closeBtn: document.querySelector('button[data-dismiss="modal"]'),
+  cardModal : document.querySelector('.gallery'),
+  imgContainer: document.querySelector('.js-modal'),
 };
 
-const { backdrop, closeBtn } = refs;
+const { backdrop, closeBtn, cardModal, imgContainer } = refs;
 
 export default function addAllEventListenersModal() {
   closeBtn.addEventListener('click', onCloseBtnClick);
@@ -38,3 +45,107 @@ function removeAllEventListenersModal() {
   window.removeEventListener('keydown', onKeydownEscape);
   backdrop.removeEventListener('click', onBackdropClick);
 }
+
+cardModal.addEventListener('click', clickOnMovieHandler);
+
+let movieId
+// клик
+function clickOnMovieHandler(e) {
+  e.preventDefault();
+
+  
+   backdrop.classList.remove('is-hidden')
+ if (e.target.nodeName !== 'IMG' && e.target.nodeName !== 'H2') {
+   return;
+ }
+
+  movieId = e.target.dataset.id;
+ 
+ 
+  fetchById(movieId);
+
+}
+
+
+
+//Фетч фильма по ID
+ function fetchById(movieId) {
+ getMovies(ID_URL).then(
+  res => {
+    renderFilmCard(res);
+    backdrop.setAttribute('id', movieId);
+    
+  }
+)
+ 
+}
+
+function renderFilmCard(film) {
+//  imgageContainer.insertAdjacentHTML('afterbegin', modalFilmCart(markup));
+modalFilmCart(film)
+ 
+}
+
+function modalFilmCart({ 
+  title, 
+  original_title,
+  vote_average, 
+  vote_count, 
+  popularity, 
+  genres, 
+  overview,
+  poster_path, } ) {
+
+  
+    const markup = `
+    <button class="button-close" data-dismiss="modal">
+            <svg class="icon" width="30" height="30" xmlns="http://www.w3.org/2000/svg">
+                <path d="m8 8 14 14M8 22 22 8" stroke-width="2" />
+            </svg>
+        </button>
+       
+        <div class="image-container">
+    <img 
+    src="${BASE_IMG_URL}${poster_path}"
+      alt="${title} movie poster}" 
+      width="240" height="357" 
+      class="image"
+      />
+    </div>
+    <div class="content">
+    <h2 class="title">${title}</h2>
+    <div class="properties">
+      <div class="titles">
+          <p class="property">Vote / Votes</p>
+          <p class="property">Popularity</p>
+          <p class="property">Original Title</p>
+          <p class="property">Genre</p>
+      </div>
+      <div class="values">
+          <p class="value"><span class="first-mark">${vote_average}</span>&nbsp;/&nbsp;<span class="second-mark">${vote_count}</span></p>
+          <p class="value">${popularity}</p>
+          <p class="value">${original_title}</p>
+          <p class="value">${genres}</p>
+      </div>
+    </div>
+    <div class="about">
+      <p class="title">About</p>
+      <div class="about-container">
+          <p class="text">${overview}</p>
+          
+          </div>
+            </div>
+            <div class="buttons">
+                <button class="button to-watched">add to Watched</button>
+                <button class="button to-queue">add to queue</button>
+            </div>
+        </div>
+    </div>
+      `;
+
+
+  
+      imgContainer.insertAdjacentHTML('afterbegin', markup);
+
+  }
+  
