f2a9a00b1f716d3daeb3a8c0df6328397c72f5d8
denkach <denkach2211@gmail.com>
2022-07-18 02:39:02 +0200

    а

--- src/js/queue.js
+++ src/js/queue.js
@@ -1,29 +1,30 @@
-import fetchMovie from './api/fetch-movie';
-import renderMovieCard from './render-movie-cards';
+export default function createQueue() {
+  const refsModal = {
+    queueBtn: document.querySelector('.to-queue'),
+    gallery: document.querySelector('.library-gallery'),
+    bg: document.querySelector('backdrop'),
+    backdrop: document.querySelector('.backdrop'),
+  };
 
-export const refsModal = {
-  queueBtn: document.querySelector('.to-queue'),
-  gallery: document.querySelector('.library-gallery'),
-  bg: document.querySelector('backdrop'),
-};
+  const { queueBtn, gallery, bg, backdrop } = refsModal;
+  const id = backdrop.id;
 
-// const { queueBtn, gallery, bg } = refs;
-// const id = backdrop.id;
-// queueBtn.addEventListener('click', onBtnQueueClick);
+  queueBtn.addEventListener('click', onBtnQueueClick);
 
-function inLocalStorage(id) {
-  if (!localStorage.getItem('queue')) {
-    return false;
+  function inLocalStorage(id) {
+    if (!localStorage.getItem('queue').contains(id)) {
+      return false;
+    }
+    return true;
   }
-  return true;
-}
 
-export function onBtnQueueClick(callback, value) {
-  if (!callback(value)) {
-    refs.queueBtn.textContent = 'Remove to queue';
-    localStorage.setItem('queue', id);
-  } else {
-    refs.queueBtn.textContent = 'Add to queue';
-    localStorage.removeItem('queue', id);
+  function onBtnQueueClick() {
+    if (!inLocalStorage(id)) {
+      refs.queueBtn.textContent = 'Remove to queue';
+      localStorage.setItem('queue', id);
+    } else {
+      refs.queueBtn.textContent = 'Add to queue';
+      localStorage.removeItem('queue', id);
+    }
   }
 }
