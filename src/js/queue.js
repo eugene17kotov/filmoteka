a36d0933b0cd4b8f92cf0f5393bcc7f54075ca22
denkach <denkach2211@gmail.com>
2022-07-18 10:21:29 +0200

    Update queue.js

--- src/js/queue.js
+++ src/js/queue.js
@@ -1,26 +1,69 @@
-const refs = {
-  queueBtn: document.querySelector('button.to-queue'),
-  gallery: document.querySelector('.library-gallery'),
+import { ID_URL, BASE_IMG_URL } from './api/api-vars';
+import { getMovies } from './api/fetch-movie';
+import { genres } from './genres.json';
+
+const refsModal = {
+  queueBtn: document.querySelector('.to-queue'),
+  library: document.querySelector('.library-gallery'),
+  bg: document.querySelector('.backdrop'),
 };
 
-const { queueBtn, gallery } = refs;
+const { queueBtn, library, bg } = refsModal;
 
-queueBtn.addEventListener('click', onBtnQueueClick);
+const localstorage = {
+  loadData(key) {
+    const localStorageData = localStorage.getItem(key);
+    return JSON.parse(localStorageData);
+  },
 
-function inLocalStorage(id) {
-  if (!localStorage.getItem('queue').contains(id)) {
+  saveData(key, value) {
+    const dataToSave = JSON.stringify(value);
+    localStorage.setItem(key, dataToSave);
+  },
+
+  setFilm(key, value) {
+    const currentCollection = this.getMovies(key);
+    currentCollection.push(value);
+    this.saveData(key, currentCollection);
+  },
+
+  removeFilm(key, value) {
+    const films = this.getMovies(key);
+    if (films.includes(value)) {
+      films.splice(films.indexOf(value), 1);
+      this.saveData(key, films);
+    } else return;
+  },
+
+  getMovies(key) {
+    const movies = this.loadData(key);
+    if (!movies) {
+      this.saveData(key, []);
+      return [];
+    } else {
+      return movies;
+    }
+  },
+};
+
+if (queueBtn) {
+  queueBtn.addEventListener('click', onBtnQueueClick);
+}
+
+function inLocalStorage(value) {
+  if (!localStorage.getItem('queue').contains(value)) {
     return false;
   }
   return true;
 }
 
 function onBtnQueueClick() {
-  const id = document.querySelector('backdrop').id;
+  const id = bg.id;
   if (!inLocalStorage(id)) {
-    queueBtn.textContent = 'Remove to queue';
-    localStorage.setItem('queue', id);
+    refs.queueBtn.textContent = 'Remove to queue';
+    localstorage.setFilm('queue', id);
   } else {
-    queueBtn.textContent = 'Add to queue';
-    localStorage.removeItem('queue', id);
+    refs.queueBtn.textContent = 'Add to queue';
+    localstorage.removeFilm('queue', id);
   }
 }
