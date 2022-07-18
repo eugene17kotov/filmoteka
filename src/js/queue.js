310b2024930d21a88320ac73b28ed6f9c7e96a71
denkach <denkach2211@gmail.com>
2022-07-18 10:13:32 +0200

    Update queue.js

--- src/js/queue.js
+++ src/js/queue.js
@@ -1,30 +1,69 @@
-export default function createQueue() {
-  const refsModal = {
-    queueBtn: document.querySelector('.to-queue'),
-    gallery: document.querySelector('.library-gallery'),
-    bg: document.querySelector('backdrop'),
-    backdrop: document.querySelector('.backdrop'),
-  };
+import { ID_URL, BASE_IMG_URL } from './api/api-vars';
+import { getMovies } from './api/fetch-movie';
+import { genres } from './genres.json';
 
-  const { queueBtn, gallery, bg, backdrop } = refsModal;
-  const id = backdrop.id;
+const refsModal = {
+  queueBtn: document.querySelector('.to-queue'),
+  library: document.querySelector('.library-gallery'),
+  bg: document.querySelector('.backdrop'),
+};
 
-  queueBtn.addEventListener('click', onBtnQueueClick);
+const localstorage = {
+  loadData(key) {
+    const localStorageData = localStorage.getItem(key);
+    return JSON.parse(localStorageData);
+  },
 
-  function inLocalStorage(id) {
-    if (!localStorage.getItem('queue').contains(id)) {
-      return false;
-    }
-    return true;
-  }
+  saveData(key, value) {
+    const dataToSave = JSON.stringify(value);
+    localStorage.setItem(key, dataToSave);
+  },
 
-  function onBtnQueueClick() {
-    if (!inLocalStorage(id)) {
-      refs.queueBtn.textContent = 'Remove to queue';
-      localStorage.setItem('queue', id);
+  setFilm(key, value) {
+    const currentCollection = this.getMovies(key);
+    currentCollection.push(value);
+    this.saveData(key, currentCollection);
+  },
+
+  removeFilm(key, value) {
+    const films = this.getMovies(key);
+    if (films.includes(value)) {
+      films.splice(films.indexOf(value), 1);
+      this.saveData(key, films);
+    } else return;
+  },
+
+  getMovies(key) {
+    const movies = this.loadData(key);
+    if (!movies) {
+      this.saveData(key, []);
+      return [];
     } else {
-      refs.queueBtn.textContent = 'Add to queue';
-      localStorage.removeItem('queue', id);
+      return movies;
     }
+  },
+};
+
+const { queueBtn, library, bg } = refsModal;
+
+if (queueBtn) {
+  queueBtn.addEventListener('click', onBtnQueueClick);
+}
+
+function inLocalStorage(value) {
+  if (!localStorage.getItem('queue').contains(value)) {
+    return false;
+  }
+  return true;
+}
+
+function onBtnQueueClick() {
+  const id = bg.id;
+  if (!inLocalStorage(id)) {
+    refs.queueBtn.textContent = 'Remove to queue';
+    localstorage.setFilm('queue', id);
+  } else {
+    refs.queueBtn.textContent = 'Add to queue';
+    localstorage.removeFilm('queue', id);
   }
 }
