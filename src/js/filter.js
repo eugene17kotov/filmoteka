07d04dfe83dbdf38361a398ff799cb980dad42ab
denkach <denkach2211@gmail.com>
2022-07-19 18:08:51 +0200

    Update filter.js

---
+++ src/js/filter.js
@@ -0,0 +1,29 @@
+const refs = {
+  btnToday: document.querySelector('button[data-group="today"]'),
+  filterBg: document.querySelector('.background__filter'),
+  btnWeek: document.querySelector('button[data-group="week"]'),
+};
+
+const { btnToday, filterBg, btnWeek } = refs;
+let anchorNodeToday = btnToday.parentNode.parentNode;
+let anchorNodeWeek = btnWeek.parentNode.parentNode;
+
+btnToday.addEventListener('click', () => {
+  if (anchorNodeToday.classList.contains('.selected')) {
+    return;
+  } else {
+    anchorNodeToday.classList.add('selected');
+    anchorNodeWeek.classList.remove('selected');
+    filterBg.classList.remove('to-right');
+  }
+});
+
+btnWeek.addEventListener('click', () => {
+  if (anchorNodeWeek.classList.contains('.selected')) {
+    return;
+  } else {
+    anchorNodeToday.classList.remove('selected');
+    anchorNodeWeek.classList.add('selected');
+    filterBg.classList.add('to-right');
+  }
+});
