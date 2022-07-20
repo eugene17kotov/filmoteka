45a114c9a5b251f6d48d8919e9952f513c3be9ef
denkach <denkach2211@gmail.com>
2022-07-20 21:31:51 +0200

    Кнопка "Вернуться к трендам"

--- src/js/filter.js
+++ src/js/filter.js
@@ -1,19 +1,34 @@
 import { renderMovieCards } from './render-movie-cards';
 import { getMovies } from './api/fetch-movie';
 import { API_KEY, BASE_URL } from './api/api-vars';
+import { onFormSubmit } from './search';
 
 const refs = {
   btnToday: document.querySelector('button[data-group="today"]'),
   filterBg: document.querySelector('.background__filter'),
   btnWeek: document.querySelector('button[data-group="week"]'),
   gallery: document.querySelector('.gallery'),
+  form: document.querySelector('.header__form'),
+  filter: document.querySelector('.filter'),
+  toTrendingBtn: document.querySelector('.to-trending__button'),
 };
 
-export const { btnToday, filterBg, btnWeek } = refs;
+export const {
+  btnToday,
+  filterBg,
+  btnWeek,
+  gallery,
+  form,
+  filter,
+  toTrendingBtn,
+} = refs;
 let anchorNodeToday = btnToday.parentNode.parentNode;
 let anchorNodeWeek = btnWeek.parentNode.parentNode;
 
 let page = 1;
+
+form.addEventListener('submit', formSubmitHandler);
+toTrendingBtn.addEventListener('click', toTrendingBtnClick);
 
 btnToday.addEventListener('click', () => {
   page = document.querySelector('.pagination-button--current').dataset.page;
@@ -47,4 +62,27 @@ function renderMovies(url) {
   getMovies(url).then(response => {
     renderMovieCards(response.results);
   });
+}
+
+function formSubmitHandler(e) {
+  filter.classList.add('is-hidden');
+  toTrendingBtn.classList.remove('is-hidden');
+}
+
+function toTrendingBtnClick() {
+  page = document.querySelector('.pagination-button--current').dataset.page;
+  const TREND_URL_WEEK = `${BASE_URL}/trending/movie/week?api_key=${API_KEY}`;
+
+  filter.classList.remove('is-hidden');
+  toTrendingBtn.classList.add('is-hidden');
+
+  if (anchorNodeWeek.classList.contains('.selected')) {
+    return;
+  } else {
+    anchorNodeToday.classList.remove('selected');
+    anchorNodeWeek.classList.add('selected');
+    filterBg.classList.add('to-right');
+    localStorage.setItem('LAST_REQUESTED_URL', TREND_URL_WEEK);
+    renderMovies(TREND_URL_WEEK + `&page=${page}`);
+  }
 }
