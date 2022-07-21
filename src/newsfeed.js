1cae81dc44ceb0370105c5a1ab709519d1edc211
veta41 <74262484+veta41@users.noreply.github.com>
2022-07-21 18:51:33 +0300

    fix

--- src/newsfeed.js
+++ src/newsfeed.js
@@ -4,7 +4,7 @@ import './sass/main.scss';
 // Scripts
 import './js/header';
 import './js/newsfeed-render';
-// import './js/team-modal';
+import './js/team-modal';
 import './js/scroll-up';
 import './js/theme-switch_newsfeed';
 
