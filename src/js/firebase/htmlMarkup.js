ef646929087523acb913c4ff804974cf1e86a3e0
Valeriia <volikavs@gmail.com>
2022-07-22 18:48:31 +0200

    fbs-modal

--- src/js/firebase/htmlMarkup.js
+++ src/js/firebase/htmlMarkup.js
@@ -1,42 +1,92 @@
-export const logregMarkup = `<div id="login-status" class="fbs-header__section">
-        <button id="tosignin"  type="button" class="fbs-header-btn">signin</button>
-        <span>  </span>
-        <button id="tosignup"  type="button" class="fbs-header-btn">signup</button>
+export const logregMarkup = `<div id="login-status" class="header__nav-item">
+        <button id="tosignin"  type="button" class="header__nav-btn">sign in</button>
+        <span class="fbs-header-span">  </span>
+        <button id="tosignup"  type="button" class="header__nav-btn">sign up</button>
       </div>`;
 
-export const loggedMarkup = `<div id="login-status">
+export const loggedMarkup = `<div id="login-status" class="header__nav-item">
         <span id="logged-user"></span>
-        <button id="logout"  type="button">logout</button>
+        <button id="log out"  type="button" class="header__nav-btn">logout</button>
       </div>`;
 
-export const loginMarkup = `<div id="user-login">
-        <button id="close-backdrop"  type="button">cancel</button>
-        <br /><br />
-        <p class="title">to log in enter your email and password</p>
+export const loginMarkup = `<div id="user-login" class="backdrop fbs-backdrop">
+        <div class="fbs-modal">
+<button id="close-backdrop"  type="button" class="fbs-mob-menu-close-btn"><svg
+            class="menu-btn__icon"
+            width="40"
+            height="40"
+            aria-label="Выключение мобильного меню"
+          >
+            <use
+              class="menu-btn__icon--close"
+              href="./././images/symbol-defs.svg#icon-close"
+            ></use>
+          </svg></button>
+
+            <p class="fbs-modal-title">To log in enter your email and password</p>
+        
         <form id="login-form">
-          <label for="email">enter valid email: </label>
-          <input type="email" name="email" required/>
-          <br />
-          <label for="email">enter password (min 6 symbols): </label>
-          <input type="password" name="password" minlength="6" required/>
-          <br /><br />
-          <button type="submit" id="fire" value="log" >go!</button>
+          <label for="email" class="fbs-modal-label">enter valid email: </label>
+          <span class="fbs-modal-label__field">
+            <input type="email" name="email" required 
+              placeholder="mail@mail.mail"
+              class="fbs-modal-label__block fbs-modal-label__block--input"/>
+              <svg class="fbs-modal-icon" width="18" height="18">
+                <use href="./images/symbol-defs.svg#email-black-18dp"></use>
+              </svg>
+          </span>
+          <label for="password" class="fbs-modal-label">enter password (min 6 symbols): </label>
+          <span class="fbs-modal-label__field">
+            <input type="password" name="password" minlength="6" required 
+              placeholder="(A-Z, a-z, 0-9, !@_-)"
+              class="fbs-modal-label__block fbs-modal-label__block--input"/>
+              <svg class="fbs-modal-icon" width="18" height="18">
+                <use href="./images/symbol-defs.svg#person-black-18dp"></use>
+              </svg>
+          </span>
+          <button type="submit" id="fire" value="log" class="btn fbs-modal-btn--submit">go!</button>
           <p id="login-error"></p>
         </form>
+        </div>
       </div>`;
 
-export const registerMarkup = `<div id="user-register">
-        <button id="close-backdrop" type="button" >cancel</button>
-        <br /><br />
-        <p class="title">to log in enter your email and password</p>
+export const registerMarkup = `<div id="user-register" class="backdrop fbs-backdrop">
+          <div class="fbs-modal">
+<button id="close-backdrop" type="button" class="fbs-mob-menu-close-btn"><svg
+            class="menu-btn__icon"
+            width="40"
+            height="40"
+            aria-label="Выключение мобильного меню"
+          >
+            <use
+              class="menu-btn__icon--close"
+              href="/src/images/symbol-defs#icon-film.svg"
+            ></use>
+          </svg></button>
+
+        <p class="fbs-modal-title">WRITE your email and password</p>
+        
         <form id="signup-form">
-          <label for="email">enter valid email: </label>
-          <input type="email" name="email" required />
-          <br />
-          <label for="email">enter password (min 6 symbols): </label>
-          <input type="password" name="password" minlength="6" required/>
-          <br /><br />
-          <button type="submit" id="fire" value="reg">go!</button>
+          <label for="email" class="fbs-modal-label">enter valid email: </label>
+          <span class="fbs-modal-label__field">
+            <input type="email" name="email" required 
+              placeholder="mail@mail.mail"
+              class="fbs-modal-label__block fbs-modal-label__block--input"/>
+              <svg class="fbs-modal-icon" width="18" height="18">
+                <use href="./images/symbol-defs.svg#email-black-18dp"></use>
+              </svg>
+          </span>
+          <label for="password" class="fbs-modal-label">enter password (min 6 symbols): </label>
+          <span class="fbs-modal-label__field">
+            <input type="password" name="password" minlength="6" required 
+              placeholder="(A-Z, a-z, 0-9, !@_-)"
+              class="fbs-modal-label__block fbs-modal-label__block--input"/>
+              <svg class="fbs-modal-icon" width="18" height="18">
+                <use href="./images/symbol-defs.svg#person-black-18dp"></use>
+              </svg>
+          </span>
+          <button type="submit" id="fire" value="reg" class="btn fbs-modal-btn--submit">go!</button>
           <p id="login-error"></p>
         </form>
+        </div>
       </div>`;
--- src/js/header.js
+++ src/js/header.js
@@ -3,6 +3,8 @@
     openMenuBtn: document.querySelector(".header-mob-menu-open-btn"),
     closeMenuBtn: document.querySelector(".header-mob-menu-close-btn"),
     menu: document.querySelector(".header-mob-menu"),
+    fbsMenuDesk: document.querySelector(".fbs--desk"),
+    fbsMenuMob: document.querySelector(".fbs--mob"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
@@ -12,4 +14,23 @@
     document.body.classList.toggle('header-menu-open');
     refs.menu.classList.toggle("visually-hidden");
   }
+
+  window.addEventListener(`resize`, resizeMenu, false);
+
+  function resizeMenu() {
+    const resizeSize = document.documentElement.clientWidth;
+    console.log('resizeMenu', typeof (resizeSize), resizeSize);
+    console.log('fbsMenuDesk.id', fbsMenuDesk.id);
+    console.log('fbsMenuMob.id', fbsMenuMob.id);
+    if (resizeSize > 767) {
+      fbsMenuDesk.id === "auth-header";
+      fbsMenuMob.id === "auth-header-2";
+    } else {
+      fbsMenuDesk.id === "auth-header-2";
+      fbsMenuMob.id === "auth-header";
+
+    }
+    
+  }
+  
 })();
