ac6203fa5bd260b7538e16d4c75fdc938b1fd7cb
Sergey Smirnov <cepera.cMupHoB@gmail.com>
2022-07-24 15:52:10 +0300

    fix

--- src/js/header.js
+++ src/js/header.js
@@ -1,17 +1,29 @@
 (() => {
   const refs = {
-    openMenuBtn: document.querySelector(".header-mob-menu-open-btn"),
-    closeMenuBtn: document.querySelector(".header-mob-menu-close-btn"),
-    menu: document.querySelector(".header-mob-menu"),
+    openMenuBtn: document.querySelector('.header-mob-menu-open-btn'),
+    closeMenuBtn: document.querySelector('.header-mob-menu-close-btn'),
+    menu: document.querySelector('.header-mob-menu'),
   };
 
-  refs.openMenuBtn.addEventListener("click", toggleMenu);
-  refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
   function toggleMenu() {
     document.body.classList.toggle('header-menu-open');
-    refs.menu.classList.toggle("visually-hidden");
+    refs.menu.classList.toggle('visually-hidden');
+    // console.log(document.getElementsByTagName('body')[0].classList[0]);
+    if (
+      document.getElementsByTagName('body')[0].classList[0] !==
+        'header-menu-open' &&
+      window.innerWidth < 767
+    ) {
+      console.log('hide');
+      document.getElementById('auth-header').classList.add('dispnone');
+    } else {
+      console.log('show');
+      document.getElementById('auth-header').classList.remove('dispnone');
+    }
+    // document.getElementById('body');
+    // document.;
   }
-
-  
 })();
