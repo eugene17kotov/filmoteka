af998df988064b7f6c1b79802af99d9dc687afef
Dmitriy Kniagnitskiy <dimak911@gmail.com>
2022-07-24 20:38:17 +0300

    fix URL

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,33 +1,34 @@
 import Glide from '@glidejs/glide';
 import { getMovies } from './api/fetch-movie';
-import { API_KEY, BASE_URL} from './api/api-vars';
+import { API_KEY, BASE_URL } from './api/api-vars';
 import { clickOnMovieHandler } from './movie-modal';
 
-const UPCOMING_URL = `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&language=en-US&page=1`;
+const UPCOMING_URL = `${BASE_URL}/movie/upcoming?api_key=${API_KEY}&region=US&language=en-US&page=1`;
 const SLIDER_IMG_URL = 'https://image.tmdb.org/t/p/w200';
-export const slideGalleryRef = document.querySelector(".glide__slides-upcoming");
+export const slideGalleryRef = document.querySelector(
+  '.glide__slides-upcoming'
+);
 
 (async () => {
   const upcomingMovieList = await getMovies(UPCOMING_URL);
 
   renderSlideMovieCards(upcomingMovieList.results);
-  
+
   const options = {
-    type: "carousel",
+    type: 'carousel',
     perView: 8,
     draggable: true,
     autoplay: 2500,
     breakpoints: {
       1280: {
-        perView: 6
+        perView: 6,
       },
       767: {
-        perView: 3
-      }
-    }
-  }
+        perView: 3,
+      },
+    },
+  };
   const slider = new Glide('.glide', options).mount();
-
 })();
 
 function renderSlideMovieCards(movies) {
@@ -36,12 +37,13 @@ function renderSlideMovieCards(movies) {
     .join('');
 
   slideGalleryRef.innerHTML = slideMovieGalleryMarkup;
-  slideGalleryRef.addEventListener("click", clickOnMovieHandler)
+  slideGalleryRef.addEventListener('click', clickOnMovieHandler);
 }
 
-function createSlideMovieMarkup (movie) {
+function createSlideMovieMarkup(movie) {
   const { title, poster_path, id, release_date } = movie;
-    poster_src = poster_path === null
+  poster_src =
+    poster_path === null
       ? 'https://dummyimage.com/100x100/000/fff.jpg&text=no+poster'
       : `${SLIDER_IMG_URL}${poster_path}`;
 
